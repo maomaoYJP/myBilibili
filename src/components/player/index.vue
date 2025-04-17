7e0d7621358394916b7ed9f927c9cd0de8fd16fb
maomao <2081498716@qq.com>
2025-04-17 20:05:24 +0800

    Reapply "引入artplayer视频库"
    This reverts commit 2563e2f0f33ef6cba4da71247e234c7310e74cb2.

---
+++ src/components/player/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="player-container"></div>
+</template>
+
+<script setup lang="ts">
+import Artplayer from "artplayer";
+import mp4 from "@/assets/video/test.mp4";
+
+onMounted(() => {
+  const art = new Artplayer({
+    container: ".player-container",
+    url: mp4,
+  });
+});
+</script>
+
+<style scoped lang="scss">
+.player-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
