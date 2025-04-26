40045ff374c5b3353152bbf09f806c34752d0be1
maomao <2081498716@qq.com>
2025-04-26 21:27:00 +0800

    完成个人页收藏页面

--- src/views/user/favlist/index.vue
+++ src/views/user/favlist/index.vue
@@ -1,7 +1,84 @@
 <template>
-  <div>收藏</div>
+  <div class="home-card-container">
+    <div class="top">
+      <span class="title">我的收藏</span>
+    </div>
+    <div class="video-card">
+      <VideoCardGroup>
+        <VideoCard v-for="item in list" :video-card="item" :key="item.title">
+          <template #desc>
+            <div class="video-card-info">
+              <a :href="'/video/123'" class="video-card-title">{{
+                item.title
+              }}</a>
+              <div class="fav-info">
+                <div class="fav-time">收藏于： {{ item.createTime }}</div>
+                <el-dropdown placement="bottom">
+                  <span class="more"
+                    ><el-icon><More /></el-icon
+                  ></span>
+                  <template #dropdown>
+                    <el-dropdown-menu>
+                      <el-dropdown-item @click="deletaFav(item)"
+                        >取消收藏</el-dropdown-item
+                      >
+                    </el-dropdown-menu>
+                  </template>
+                </el-dropdown>
+              </div>
+            </div>
+          </template>
+        </VideoCard>
+      </VideoCardGroup>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import VideoCardGroup from "@/components/video-card/video-card-group.vue";
+import VideoCard from "@/components/video-card/index.vue";
+import { getList } from "@/api/video";
+import type { video } from "@/api/video/type";
 
-<style scoped></style>
+const list = ref<video[]>([]);
+
+onMounted(async () => {
+  const res = await getList(1, 8);
+  list.value = res.data.videoList;
+});
+
+const deletaFav = (item: video) => {
+  list.value = list.value.filter((video) => video.title != item.title);
+};
+</script>
+
+<style scoped lang="scss">
+.home-card-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: $l-margin;
+  .top {
+    .title {
+      font-size: $m-text-size;
+    }
+  }
+  .video-card {
+    margin-top: $l-margin;
+    .video-card-info {
+      font-size: 14px;
+      color: $text-color-deep;
+      .fav-info {
+        margin-top: $s-margin;
+        display: flex;
+        justify-content: space-between;
+        .more {
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+        }
+      }
+    }
+  }
+}
+</style>
